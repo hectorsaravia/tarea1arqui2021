2e2581e7df5b006931ec5af4e96e3e81c00968e5
hectorsaravia <hector.saravia@mail.udp.cl>
2021-09-24 09:40:21 -0300

    Añadido backend punto de entrada server.js

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+//se declara la aplicación de expressjs
+const app = express();
+
+//set puerto dado en el env o por defecto en 8081
+const PORT = process.env.PORT || 8081;
+
+//pasa contenido a través de json
+app.use(express.json());
+
+//pasa contenido del tipo x-www-form-urlencoded de un  tamaño mayor al por defecto
+app.use(express.urlencoded({ extended: true }));
+
+//error 500 handler
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    console.error(err.message, err.stack);
+    res.status(statusCode).json({'message': err.message});
+  
+    return;
+});
+
+//error 400 handler
+app.use( (req, res, next) => {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+//ruta de ejemplo
+app.get("/", (req, res) => {
+    res.json({ message: "prueba proyecto tarea 1 arqui" });
+});
+
+//la app empieza a escuchar en el puerto declarado antes
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
