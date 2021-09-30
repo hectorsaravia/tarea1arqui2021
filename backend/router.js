36cbea008be6dcb27f88862f2b5a21aa7b3523bf
hectorsaravia <hector.saravia@mail.udp.cl>
2021-09-30 15:49:10 -0300

    Añadidas rutas de login y registro

--- backend/router.js
+++ backend/router.js
@@ -2,13 +2,43 @@ const express = require('express');
 const router = express.Router();
 
 //importar queries de base de datos
-const {test }= require('./queries');
+const {
+    test,
+    login,
+    register,
+} = require('./queries');
 
 //ruta de ejemplo
 router.get("/test", (req, res) => {
+    test(function (result) {
+        res.json({result: result})
+    });
 
-    test(result => res.json({result: result}));
+});
 
+//ruta de login
+router.post("/login", (req, res) => {
+    const data = req.body;
+    login(data, function (result) {
+        if (result.rowCount > 0) {
+            res.json({message: result.rows[0]});
+        } else {
+            res.json({message: false});
+        };
+    });
+});
+
+//ruta de registro
+router.post("/registro", (req, res) => {
+    const data = req.body;
+    register(data, function (result) {
+        if (result.rowCount > 0) {
+            res.json({message: true});
+        } else {
+            res.json({message: false});
+            console.log(result.detail);
+        }
+    });
 });
 
 module.exports = router;
