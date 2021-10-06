965061fab13ea7820e77dbeef9f1101c29f72972
christian tapia <tapia.christian.ig@gmail.com>
2021-10-06 16:15:32 -0300

    cambios sql y falta añadir cors

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,15 +1,15 @@
-CREATE TABLE IF NOT EXIST Usuarios (
+CREATE TABLE IF NOT EXISTS Usuarios (
     nombre varchar(250) NOT NULL,
     contrasena varchar(250) NOT NULL,
     PRIMARY KEY (nombre)
 );
-CREATE TABLE IF NOT EXIST Comida (
+CREATE TABLE IF NOT EXISTS Comida (
     nombre varchar(250) NOT NULL,
     descripcion TEXT NOT NULL,
     rutaImagen varchar(250) NOT NULL,
     PRIMARY KEY (nombre)
 );
-CREATE TABLE IF NOT EXIST Resenas (
+CREATE TABLE IF NOT EXISTS Resenas (
     id INT NOT NULL,
     votacion FLOAT NOT NULL,
     descripcion TEXT NOT NULL,
