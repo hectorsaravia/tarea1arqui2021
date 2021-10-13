5a2413d97c8af18481fd0cdcef06fb05d08781a0
hectorsaravia <hector.saravia@mail.udp.cl>
2021-10-13 16:15:15 -0300

    Modificados router links

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <router-link to="/">Login</router-link>
-    <router-link to="/Registro">Registro</router-link>
+    <div v-if="!this.$session.exists()">
+      <router-link to="/">Ingresar</router-link> | 
+      <router-link to="/Registro">Registrarse</router-link>
+    </div>
+    <div v-else>
+      <router-link to="/logout"></router-link>
+    </div>
     <router-view></router-view>
   </div>
 </template>
