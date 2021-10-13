f8379d5ab514a050d4165458ab6ab2786987a09d
hectorsaravia <hector.saravia@mail.udp.cl>
2021-10-13 16:15:27 -0300

    Añadida ruta dashboard

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,12 +3,14 @@ import VueRouter from 'vue-router'
 
 import Login from '../views/Login.vue';
 import Registro from '../views/Registro.vue';
+import Dashboard from '../views/Dashboard.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {path: '/', component: Login},
-  {path: '/registro', component: Registro}
+  {path: '/registro', component: Registro},
+  {path: '/dashboard', component: Dashboard}
 ]
 
 const router = new VueRouter({
