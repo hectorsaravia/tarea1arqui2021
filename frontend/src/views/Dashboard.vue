0220eefb46f0e1417e7f6992528ee726ec9bacc4
hectorsaravia <hector.saravia@mail.udp.cl>
2021-10-14 20:22:47 -0300

    Modificado colores de dashboard

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,24 +1,24 @@
 <template>
-  <div>
+  <div id="dashboard">
+    <router-link class="btn btn-danger" to="/newcomida">Ingresar Comida</router-link>
     <Comidas v-for="c in values" v-bind:key="c" v-bind:nombre_comida="c.nombre" v-bind:desc_comida="c.descripcion" >
-      <Resenas v-bind:nombre_comida="c.nombre"/>
+      
     </Comidas>
   </div>
 </template>
 
 <script>
 import Comidas from '../components/Comidas.vue'
-import Resenas from '../components/Resenas.vue'
 export default {
     name: 'Dashboard',
     data() {
           return {
-            values:[]
+            values:[],
+            renderReview: false
           };
         },
         components: {
-          Resenas,
-          Comidas
+          Comidas,
         },
         methods: {
             getComidas(){
@@ -42,5 +42,7 @@ export default {
 </script>
 
 <style>
-
+#dashboard {
+  height: 100%
+}
 </style>
