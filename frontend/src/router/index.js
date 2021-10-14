907fe6fcfa01dca78747b6e52772d4569220ecb5
hectorsaravia <hector.saravia@mail.udp.cl>
2021-10-14 20:22:18 -0300

    Añadida ruta de detalle de comidas

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,13 +4,16 @@ import VueRouter from 'vue-router'
 import Login from '../views/Login.vue';
 import Registro from '../views/Registro.vue';
 import Dashboard from '../views/Dashboard.vue';
-
+import NewComida from '../views/NewComida.vue';
+import ComidaDetalle from '../views/ComidaDetalle.vue'
 Vue.use(VueRouter);
 
 const routes = [
   {path: '/', component: Login},
   {path: '/registro', component: Registro},
-  {path: '/dashboard', component: Dashboard}
+  {path: '/dashboard', component: Dashboard},
+  {path: '/newcomida', component: NewComida},
+  {path: '/comida/:nombre', component:ComidaDetalle, name:"ComidaDetalle"},
 ]
 
 const router = new VueRouter({
