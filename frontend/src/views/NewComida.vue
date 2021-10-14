9e7b6b21d72c18616718631d7def54cf4f1a15f2
ChristianIgn <christian.tapia@mail.udp.cl>
2021-10-14 17:19:10 -0300

    Avance

---
+++ frontend/src/views/NewComida.vue
@@ -0,0 +1,32 @@
+<template>
+  
+  
+</template>
+
+<script>
+
+export default {
+    name: 'Dashboard',
+    data() {
+          return {
+            votacion: 0
+          };
+        },
+        components: {
+         
+        },
+        methods: {
+
+        },
+        computed: {
+
+        },
+        created(){
+
+        }
+}
+</script>
+
+<style>
+
+</style>
