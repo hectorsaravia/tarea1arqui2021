915bab17a690392eaf0b011cf096161ae617e595
hectorsaravia <hector.saravia@mail.udp.cl>
2021-10-14 20:21:42 -0300

    Añadido formulario para reseñas

--- frontend/src/components/HacerResena.vue
+++ frontend/src/components/HacerResena.vue
@@ -1,29 +1,41 @@
 <template>
     <div>
         <h6 class="card-header">Ingresar una nueva reseña</h6>
-        <form class="form-group">
-            <input v-model="descripcion" type="text" class="form-control" placeholder="Una breve descripción de su reseña" required>
-            <button type="button" class="btn btn-danger" @click="doResena()">Ingresar</button><br>
-        </form>
+        <div style="padding: 20px">
+            <div class="col-lg-4 col-md-6 col-sm-8 mx-auto " style="border-style: solid; border-width: medium; border-radius: 10px; border-color: white;">
+                <form class="form-group">
+                    <div style= "display: flex; text-align: left; justify-content: center; flex-direction: column;">
+                        <StarRating v-model="valoracion"/>
+                    </div>
+                    <input v-model="descripcion" type="text" class="form-control" placeholder="Una breve descripción de su reseña" required>
+                    <button type="button" class="btn btn-danger" @click="doResena()">Ingresar</button><br>
+                </form>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import StarRating from 'vue-star-rating'
+
 export default {
     name: 'HacerResena',
+    components: {
+        StarRating
+    },
     data() {
         return {
-            valoracio: "",
+            valoracion: "",
             descripcion: ""
         }
     },
     methods:{
         async doResena() {
             this.$axios.post("http://localhost:8081/api/new_resena", {
-                valoracion: this.valoracion,
-                nombre_usuario: this.descripcion,
+                votacion: this.valoracion,
+                descripcion: this.descripcion,
                 nombre_comida: this.nombre_comida,
-                nombre_usuario: this.nombre_usuario,
+                nombre_usuario: localStorage.name,
             }).then(response => {
                 if(!response.data.message) {
                     alert("No se ha podido enviar su reseña");
@@ -36,7 +48,6 @@ export default {
     },
     props: {
         nombre_comida: String,
-        nombre_usuario: String
     },
 }
 </script>
